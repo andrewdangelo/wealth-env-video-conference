bcb06c871fade3f3a2164053d1c6d94c1d365530
Andrew D'Angelo <andrew@ecuremeeting.org>
2022-01-11 17:58:44 -0500

    added webcam, mic, and share buttons

--- frontend/src/components/Controls/ControlBar.js
+++ frontend/src/components/Controls/ControlBar.js
@@ -7,6 +7,9 @@ import styles from "./controlBar.module.css";
 
 function ControlBar() {
     const [webcamOff, setWebcamOff] = useState(false);
+    const [micOff, setMicOff] = useState(false);
+    const [shareOff, setShareOff] = useState(false);
+
 
     const sfu = useContext(RoomContext);
 
@@ -20,13 +23,38 @@ function ControlBar() {
         else {
           setWebcamOff(true);
           sfu.disableWebcam();
-        } 
-        
+        }    
+    }
+
+    const handleMic = () => {
+        if (micOff){
+            setMicOff(false);
+            sfu.enableMic();
+        } else {
+            setMicOff(true);
+            sfu.disableMic();
+        }
+    }
+
+    const handleShare = () => {
+        if (shareOff){
+            setShareOff(false);
+            sfu.enableShare();
+        } else {
+            setShareOff(true);
+            sfu.disableShare();
+        }
     }
     return (
         <div className={ styles.container}>
             <div>
                 {webcamOff ? (<button onClick={handleWebcam}>Turn webcam on</button>) : <button onClick={handleWebcam}>Turn webcam Off</button>}
+            </div>
+            <div>
+                {micOff ? (<button onClick={handleMic}>Turn mic on</button>) : <button onClick={handleMic}>Turn mic Off</button>}
+            </div>
+            <div>
+                {shareOff ? (<button onClick={handleShare}>Turn share on</button>) : <button onClick={handleShare}>Turn share Off</button>}
             </div>
         </div>
     )
